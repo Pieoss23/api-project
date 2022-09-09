82bd61d1f061a5459f923f316b78bb33b282272f
Pietro Oss <pietrooss92@gmail.com>
2022-09-09 11:27:23 +0200

    add supertest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,39 @@
 module.exports = {
-    root: true,
     env: {
+        browser: true,
         node: true,
+        jest: true,
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
     overrides: [
         {
             files: ["*.ts"],
             rules: {
-                "no-undef": "off",
+                "no-undef": [
+                    "off",
+                    {
+                        typeof: false,
+                    },
+                ],
             },
         },
         {
             files: ["*.test.ts"],
+            rules: {},
             env: {
-                node: true,
                 jest: true,
+                node: true,
             },
         },
     ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
     rules: {
-        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/ban-ts-comment": ["off"],
+        "@typescript-eslint/no-unused-vars": ["off"],
     },
 };
