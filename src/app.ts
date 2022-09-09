fe240fe0c36d95d90b002d3154dd31b9b5cc217a
Pietro Oss <pietrooss92@gmail.com>
2022-09-09 11:57:54 +0200

    add GET /planets route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { response } from "express";
 import "express-async-errors";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send("This is a space facts to API");
+app.get("/planets", (request, response) => {
+    const planets = [
+        { name: "Mercury" },
+        { name: "Venus" },
+        // { name: "Jupiter" },
+    ];
+    response.json(planets);
 });
 export default app;
