f0e61ad126e2bc996a496a7969abeeab912ca0e7
Pietro Oss <pietrooss92@gmail.com>
2022-09-29 12:01:56 +0200

    configure Github authentication strategy

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import express from "express";
 import "express-async-errors";
 import { validationErrorMiddleware } from "./lib/middleware/validation";
 import { initCorsMiddleware } from "./lib/middleware/cors";
-import { initSessionMidlleware } from "./lib/middleware/session";
+import { initSessionMiddleware } from "./lib/middleware/session";
 import { passport } from "./lib/middleware/passport";
 import planetsRoutes from "./routes/planets";
 
 const app = express();
 
-app.use(initSessionMidlleware());
+app.use(initSessionMiddleware());
 
 app.use(passport.initialize());
 app.use(passport.session());
